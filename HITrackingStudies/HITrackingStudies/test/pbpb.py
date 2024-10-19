b71daab1e4495580ec98e3e49c8b894b99f723e6
Raghunath Pradhan <raghunath.pradhan@cern.ch>
2024-10-19 18:36:26 +0200

    For 2024 HI run Ntuple test

---
+++ HITrackingStudies/HITrackingStudies/test/pbpb.py
@@ -0,0 +1,43 @@
+pbpb_mc_recodebug=[
+    #~~~~~~~~~~~~~For 2023~~~~~~~~~~~~~~~
+    'root://cmsxrootd.fnal.gov//store/user/sarteaga/MinBias_Drum5F_5p36TeV_hydjet/MinBias_PbPb_5p36TeV_Hydjet_RECODEBUG_132X_v9/241010_191615/0000/MinBias_PbPb_5p36TeV_Hydjet_132X_v9_RECODEBUG_1.root',
+    'root://cmsxrootd.fnal.gov//store/user/sarteaga/MinBias_Drum5F_5p36TeV_hydjet/MinBias_PbPb_5p36TeV_Hydjet_RECODEBUG_132X_v9/241010_191615/0000/MinBias_PbPb_5p36TeV_Hydjet_132X_v9_RECODEBUG_2.root',
+    'root://cmsxrootd.fnal.gov//store/user/sarteaga/MinBias_Drum5F_5p36TeV_hydjet/MinBias_PbPb_5p36TeV_Hydjet_RECODEBUG_132X_v9/241010_191615/0000/MinBias_PbPb_5p36TeV_Hydjet_132X_v9_RECODEBUG_3.root',
+    'root://cmsxrootd.fnal.gov//store/user/sarteaga/MinBias_Drum5F_5p36TeV_hydjet/MinBias_PbPb_5p36TeV_Hydjet_RECODEBUG_132X_v9/241010_191615/0000/MinBias_PbPb_5p36TeV_Hydjet_132X_v9_RECODEBUG_4.root',
+    'root://cmsxrootd.fnal.gov//store/user/sarteaga/MinBias_Drum5F_5p36TeV_hydjet/MinBias_PbPb_5p36TeV_Hydjet_RECODEBUG_132X_v9/241010_191615/0000/MinBias_PbPb_5p36TeV_Hydjet_132X_v9_RECODEBUG_5.root'
+
+    #~~~~~~~~~~~~~For 2024~~~~~~~~~~~~~~~
+    #'file:/eos/cms/store/group/phys_heavyions/cbennett/crabSubmit_RECODEBUG_HYDJET_5360GeV_141X_2024-09-23/HYDJET_5360GeV_141X/HYDJET_5360GeV_RECODEBUG_141X_2024-09-23/240923_194854/0000/HYDJET_5360GeV_RECODEBUG_7.root',
+]
+
+pbpb_mc_reco_aod=[
+    #~~~~~~~~~~~~~For 2023~~~~~~~~~~~~~~~
+    'root://cmsxrootd.fnal.gov//store/mc/HINPbPbSpring23Reco/MinBias_Drum5F_5p36TeV_hydjet/AODSIM/NoPU_132X_mcRun3_2023_realistic_HI_v9-v2/2820000/00233439-15e0-406b-9363-da127ed271c0.root',
+    'root://cmsxrootd.fnal.gov//store/mc/HINPbPbSpring23Reco/MinBias_Drum5F_5p36TeV_hydjet/AODSIM/NoPU_132X_mcRun3_2023_realistic_HI_v9-v2/2820000/02464949-cb4c-4b6e-a2e9-894f195ddec3.root'
+
+    #~~~~~~~~~~~~~For 2024~~~~~~~~~~~~~~~
+]
+
+pbpb_mc_miniaod=[
+    #~~~~~~~~~~~~~For 2023~~~~~~~~~~~~~~~
+    'root://cmsxrootd.fnal.gov//store/mc/HINPbPbSpring23MiniAOD/MinBias_Drum5F_5p36TeV_hydjet/MINIAODSIM/NoPU_132X_mcRun3_2023_realistic_HI_v9-v2/2820000/02288831-b588-4380-bac8-9910f24691bd.root',
+    'root://cmsxrootd.fnal.gov//store/mc/HINPbPbSpring23MiniAOD/MinBias_Drum5F_5p36TeV_hydjet/MINIAODSIM/NoPU_132X_mcRun3_2023_realistic_HI_v9-v2/2820000/03f9d7b4-d340-47e0-9cf7-ce3596e0fc8c.root',
+    '/store/mc/HINPbPbSpring23MiniAOD/MinBias_Drum5F_5p36TeV_hydjet/MINIAODSIM/NoPU_132X_mcRun3_2023_realistic_HI_v9-v2/2820000/0509b077-65db-403f-979f-dd689754cfbd.root'
+    
+    #~~~~~~~~~~~~~For 2024~~~~~~~~~~~~~~~
+]
+
+pbpb_data_reco_aod=[
+]
+
+pbpb_data_miniaod=[
+    #~~~~~~~~~~~~~For 2023~~~~~~~~~~~~~~~
+    'root://cmsxrootd.fnal.gov//store/hidata/HIRun2023A/HIPhysicsRawPrime0/MINIAOD/PromptReco-v2/000/374/668/00000/06179488-b7e6-44f6-bec9-eb242a290ffd.root',
+    'root://cmsxrootd.fnal.gov//store/hidata/HIRun2023A/HIPhysicsRawPrime0/MINIAOD/PromptReco-v2/000/374/668/00000/119881a4-1fdd-4462-818e-55b3ab3962f5.root'
+
+    #~~~~~~~~~~~~~For 2024~~~~~~~~~~~~~~~
+]
+
+pbpb_data_reco_aod_streams=[
+    'file:/eos/cms/store/t0streamer/Data/HIPhysicsMinimumBias0/000/325/174/run325174_ls0001_streamHIPhysicsMinimumBias0_StorageManager.dat'
+]
